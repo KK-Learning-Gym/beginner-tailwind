d8b8635affe5fde77443b05961dd4c1037b2861a
tinfoil-knight <kunal99kundu@gmail.com>
2020-09-13 19:34:59 +0530

    fix: made ModuleListItem, EmojiCard responsive

--- src/components/EmojiCard.js
+++ src/components/EmojiCard.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function EmojiCard({ name, bg }) {
 	return (
-		<div className="flex items-center bg-white rounded overflow-hidden w-3/12">
+		<div className="flex items-center bg-white rounded overflow-hidden">
 			<div className={bg + ' py-4 px-8 text-2xl'}>
 				<span role="img" aria-label="heart-eyes">
 					😍
 				</span>
 			</div>
-			<div className="text-gray-600 py-4 pl-6">{name}</div>
+			<div className="text-gray-600 py-4 px-6">{name}</div>
 		</div>
 	)
 }
